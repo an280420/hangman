ebe6cd27b099aa09f90edc88bdc612c0e336f3fa
@an280420 <angit280420@gmail.com>
2021-01-05 11:50:15 +0500

    edit codestyle

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -2,9 +2,9 @@ require 'colorize'
 
 class ConsoleInterface
   FIGURES =
-      Dir[__dir__ + '/../data/figures/*.txt'].
-      sort.
-      map { |file_name| File.read(file_name) }
+    Dir[__dir__ + '/../data/figures/*.txt'].
+    sort.
+    map { |file_name| File.read(file_name) }
 
   def initialize(game)
     @game = game
